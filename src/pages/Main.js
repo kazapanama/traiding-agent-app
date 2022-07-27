0c6c8212bb92e340c0d371405e3a4f524b9e7073
Vova <kazap3r@gmail.com>
2022-07-27 12:00:52 +0300

    main page items are now from db

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,37 @@
 import Coffee from "../components/Coffee/Coffee";
 
+import { useState, useEffect } from "react";
+
+import {db} from '../firebase'
+import { collection, doc, onSnapshot} from 'firebase/firestore'
+
+
+
 const Main = () => {
+
+    const [products, setProducts] = useState({})
+   
+
+    const productsCollectionRef = collection(db,'products')
+
+
+    useEffect(() => {
+        const getProducts = async () =>{
+          
+          await onSnapshot(productsCollectionRef,(data)=>{setProducts(data.docs.map((doc)=>({...doc.data(),id:doc.id})))})
+          
+        }
+    
+        getProducts()
+      },[])
+
+
+
+
     return ( 
         <>
             <h1>main page </h1>
-            < Coffee />
+            < Coffee products={products}/>
         </>
         
      );
